6bb6235643d7a03dfec939ed1a8c0d78eaa97873
lateefay <hammeda582@gmail.com>
2026-03-21 18:22:52 +0100

    reconstructed header and hero section and other touches

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 //  mobile navition show and hide toggle
+const mobileLink= document.querySelector(".mobile-link");
+const mobileNav= document.querySelector(".mobile-nav-cont");
 
-let toggle = document.querySelector(".toggle-bar");
-let hideBar= document.querySelector(".hambuger-cont ul")
 
+mobileNav.addEventListener("click", (e)=>{
+    if(!e.target.classList.contains("fa-bars")) return;
+    mobileLink.classList.toggle("active-mobile"); 
 
-toggle.addEventListener("click", () => {
-    hideBar.classList.toggle("show")
+        e.stopPropagation(); 
+
 })
 
 
-// document.querySelectorAll(".close-nav-with-body-click").addEventListener("click", () => {
-//     document.querySelector(".mobile-links").style.display = "none";
-//     console.log("i clicked hero");
-
-// })
-
+document.addEventListener("click", (e) => {
+    // If the menu is open AND the user clicked outside the menu
+    if (mobileLink.classList.contains("active-mobile") && !mobileLink.contains(e.target)) {
+        mobileLink.classList.remove("active-mobile");
+    }
+});
